174d39794a3afdb3916c3c1cd719d80c3f6f5081
stanley450 <landon.stanley@yahoo.com>
2015-01-21 22:37:26 -0600

    first person to third person and animations

---
+++ Assets/ViewChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
