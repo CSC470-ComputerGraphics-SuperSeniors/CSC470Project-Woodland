225ed2d711d6049762264e1a20dccffb39f6b281
stanley450 <landon.stanley@yahoo.com>
2015-02-05 08:51:00 -0600

    punching and kicking working with mouse buttons

---
+++ Assets/MouseActions.js
@@ -0,0 +1,28 @@
+﻿public var punchAnimation : AnimationClip;
+public var kickAnim : AnimationClip;
+
+function Start () {
+	animation[punchAnimation.name].wrapMode = WrapMode.Once;
+	animation[punchAnimation.name].layer = 1;
+	
+	animation[kickAnim.name].wrapMode = WrapMode.Once;
+	animation[kickAnim.name].layer = 1;
+}
+
+var flag;
+function Update () {
+
+	 if (animation.IsPlaying(punchAnimation.name) || animation.IsPlaying(kickAnim.name)){
+	      flag = true;
+	   } else {
+	       flag = false; }
+
+	if(Input.GetMouseButtonDown(0) && !flag) {
+	
+	animation.Play(punchAnimation.name); 
+	
+	}
+	if(Input.GetMouseButtonDown(1) && !flag) {
+	animation.Play(kickAnim.name); 
+	}
+}
